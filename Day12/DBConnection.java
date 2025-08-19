44f05089a52510a95e5ecd73a655b3a17f68ec47
Likhitha934 <gnlikhitha67@gmail.com>
2025-08-19 21:48:30 +0530

    Add files via upload

---
+++ Day12/DBConnection.java
@@ -0,0 +1,16 @@
+package JdbC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/hospital_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Likhitha@21";
+
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
